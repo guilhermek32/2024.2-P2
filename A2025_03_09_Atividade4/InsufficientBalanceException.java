40b1bd689b49c0642e12faf8d90888312920df53
Antonio Guilherme da Silva <guilhermek32@gmail.com>
2025-03-04 16:57:35 -0300

    Delete A2025_03_09_Atividade4 directory

--- A2025_03_09_Atividade4/InsufficientBalanceException.java
+++
@@ -1,7 +0,0 @@
-package A2025_03_09_Atividade4;
-
-public class InsufficientBalanceException extends RuntimeException {
-    public InsufficientBalanceException(String message) {
-        super(message);
-    }
-}
--- A2025_03_09_Atividade4/WithdrawLimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package A2025_03_09_Atividade4;
-
-public class WithdrawLimitExceededException extends RuntimeException {
-    public WithdrawLimitExceededException(String message) {
-        super(message);
-    }
-}
